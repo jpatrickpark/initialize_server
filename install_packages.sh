d5401de2911b4e2d7717a34c3f885376cae6a70f
JP <jp@jpatrickpark.com>
2016-09-16 12:07:13 -0700

    added a script to install packages along with ctags package

---
+++ install_packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# installs pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# installs vim-go-ide
+git clone git@github.com:farazdagi/vim-go-ide.git ~/.vim_go_runtime
+sh ~/.vim_go_runtime/bin/install
+echo "alias vimgo='vim -u ~/.vimrc.go'" >> .profile
+
+# installs gcc
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential
+
+# installs ctags
+tar zxvf ctags-5.8.tar.gz
+cd ctags-5.8
+./configure
+make
+make install
